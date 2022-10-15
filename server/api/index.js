65dbe108db6d5b55d4ba95e7969322fb9bb4a00c
Christopher Milan <chris.m.milan@gmail.com>
2022-10-15 16:13:18 -0700

    server api scaffolding

---
+++ server/api/index.js
@@ -0,0 +1,9 @@
+import practice from "./practice";
+import signup from "./signup";
+import users from "./users";
+
+export default {
+  practice,
+  signup,
+  users
+}
--- server/app.js
+++ server/app.js
@@ -1,4 +1,7 @@
-const express = require("express");
+import express from "express";
+
+import routers from "./api/index"
+
 const app = express();
 const port = 8080;
 
@@ -6,7 +9,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(port, () => {
-  console.log(`Started server on port http://localhost:${port}/`)
-})
+app.use("/practice", routers.practice);
+app.use("/signup", routers.signup);
+app.use("/users", routers.users);
 
+app.listen(port, () => {
+  console.log(`Started server on port http://localhost:${port}/`);
+});

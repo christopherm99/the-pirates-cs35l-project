3af75cdce2aef730a5eb4afa89a1f78d1afe77ad
Christopher Milan <chris.m.milan@gmail.com>
2022-10-16 14:32:26 -0700

    refactor mysql into db.js

--- server/app.js
+++ server/app.js
@@ -4,8 +4,6 @@ import express from "express";
 import api from "./api/index.js";
 import authRouter from "./routes/auth.js";
 
-import * as mysql from "mysql";
-
 const app = express();
 const port = 8080;
 
@@ -18,53 +16,7 @@ app.use("/practice", api.practice);
 app.use("/signup", api.signup);
 app.use("/users", api.users);
 
-
 // putting sql stuff here for now, we can move it to a different module later if desired
-//////////////// Connection and Set Up ////////////////////////////////////////////////////////////////
-var con; //connection to sql
-const sql_db = "X_MARKS_SPOT";
-// connect to SQL if anybody is using it
-
-connectLocalSQL();
-
-// creates a connection to the SQL database
-async function connectLocalSQL() {
-    let sql_user, sql_pass;
-
-    // todo put conditional statements here if you're using different local mysqls
-    sql_user = process.env["CAESAR_SQL_USER"];
-    sql_pass = process.env["CAESAR_SQL_PASS"];
-
-    // it is necessary to make a pool, because mysql gets upset sometimes if it runs for too long
-    con = mysql.createPool({
-        connectionLimit: 10,
-        host: "localhost",
-        user: sql_user,
-        password: sql_pass,
-        database: sql_db
-    });
-
-    /*
-    con.connect(function (err) {
-        if (err) throw err;
-        console.log("Connected to local SQL!");
-        connectedToLocalSQL = true;
-    });
-    */
-    con.on('connection', function (connection) {
-        console.log('DB Connection established');
-
-        connection.on('error', function (err) {
-            console.error(new Date(), 'MySQL error', err.code);
-        });
-        connection.on('close', function (err) {
-            console.error(new Date(), 'MySQL close', err);
-        });
-
-    });
-}
-
-
 
 app.listen(port, () => {
   console.log(`Started server on port http://localhost:${port}/`);
---
+++ server/db.js
@@ -0,0 +1,36 @@
+import * as mysql from "mysql";
+
+//////////////// Connection and Set Up ////////////////////////////////////////////////////////////////
+const sql_db = "X_MARKS_SPOT";
+// todo put conditional statements here if you're using different local mysqls
+const sql_user = process.env["CAESAR_SQL_USER"];
+const sql_pass = process.env["CAESAR_SQL_PASS"];
+
+// it is necessary to make a pool, because mysql gets upset sometimes if it runs for too long
+const con = mysql.createPool({
+  connectionLimit: 10,
+  host: "localhost",
+  user: sql_user,
+  password: sql_pass,
+  database: sql_db,
+});
+
+/*
+  con.connect(function (err) {
+      if (err) throw err;
+      console.log("Connected to local SQL!");
+      connectedToLocalSQL = true;
+  });
+  */
+con.on("connection", function (connection) {
+  console.log("DB Connection established");
+
+  connection.on("error", function (err) {
+    console.error(new Date(), "MySQL error", err.code);
+  });
+  connection.on("close", function (err) {
+    console.error(new Date(), "MySQL close", err);
+  });
+});
+
+export default con;

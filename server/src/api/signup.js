67a77a62b93d733572e8ff89b1bb26dd329dbc2f
Christopher Milan <chris.m.milan@gmail.com>
2022-10-20 16:07:37 -0700

    server: implement /api/signup

--- server/src/api/signup.js
+++ server/src/api/signup.js
@@ -1,11 +1,25 @@
 import express from "express";
+
+import db from "../../db.js";
 import { requiresAuth } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/", requiresAuth, (req, res) => {
-  console.log(res.body);
-  res.status(200).send("OK");
+  console.log(req.body);
+  let queries = [];
+  req.body.days_to_practice.forEach((time) => {
+    queries.push(
+      db.query(
+        "INSERT INTO sign_ups (user_id, timestamp, car_capacity, leave_time) VALUES (?, ?, ?, ?)",
+        [req.user.user_id, Date.now(), req.body.car_capacity, Date(time)]
+      )
+    );
+  });
+  Promise.all(promises).then(() => {
+    res.status(200).send("OK");
+    // TODO: refresh practices table
+  });
 });
 
 export default router;

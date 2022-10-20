20f39e398415da5646aaf903bdf620b707039dcf
Michael Bunte <michaelbunte@Michaels-MacBook-Pro.local>
2022-10-19 19:55:06 -0700

    Made input form pretty

--- client/src/form/Form.js
+++ client/src/form/Form.js
@@ -38,114 +38,121 @@ export default function CalendarPage() {
   return (
     <>
       <Navbar returnHomeButton loginButton/>
-      <div>
-        Which days can you come?
-      </div>
-      {dayNames.map((dayName, index) => {
-        return (
+      <div className="form">
+        <div className="main-question">
           <div>
-            <label>
-              <input type="checkbox" checked={daysChecked[index]} onChange={() => {toggleDay(index)}}/>
-              {dayName}
-            </label>
-
-            
-            {daysChecked[index] && 
-              <>
-                <div className="form-check">
-                  <label>
-                    <input
-                      type="radio"
-                      value="option1"
-                      checked={selectedTimes[index] === "2:00"}
-                      onChange={() => handleDayTimeUpdate(index, "2:00")}
-                      className="form-check-input"
-                    />
-                    2:00
-                  </label>
-                </div>
-
-                <div className="form-check">
-                  <label>
-                    <input
-                      type="radio"
-                      value="2:15"
-                      checked={selectedTimes[index] === "2:15"}
-                      onChange={() => handleDayTimeUpdate(index, "2:15")}
-                      className="form-check-input"
-                    />
-                    2:15
-                  </label>
-                </div>
-
-                <div className="form-check">
-                  <label>
-                    <input
-                      type="radio"
-                      value="2:30"
-                      checked={selectedTimes[index] === "2:30"}
-                      onChange={() => handleDayTimeUpdate(index, "2:30")}
-                      className="form-check-input"
-                    />
-                    2:30
-                  </label>
-                </div>
-              </>
-            }
+            Which days can you come?
           </div>
-        )
-      })}
+          {dayNames.map((dayName, index) => {
+            return (
+              <div>
+                <label>
+                  <input type="checkbox" className="check-day" checked={daysChecked[index]} onChange={() => {toggleDay(index)}}/>
+                  {dayName}
+                </label>
 
-      <div>
-        <div>
-          Can you drive?
+                
+                {daysChecked[index] && 
+                  <div className="form-check mini-question">
+                    <>
+                      <label>
+                        <input
+                          type="radio"
+                          value="option1"
+                          checked={selectedTimes[index] === "2:00"}
+                          onChange={() => handleDayTimeUpdate(index, "2:00")}
+                          className="form-check-input"
+                        />
+                        2:00
+                      </label>
+                    </>
+
+                    <div className="form-check">
+                      <label>
+                        <input
+                          type="radio"
+                          value="2:15"
+                          checked={selectedTimes[index] === "2:15"}
+                          onChange={() => handleDayTimeUpdate(index, "2:15")}
+                          className="form-check-input"
+                        />
+                        2:15
+                      </label>
+                    </div>
+
+                    <div className="form-check">
+                      <label>
+                        <input
+                          type="radio"
+                          value="2:30"
+                          checked={selectedTimes[index] === "2:30"}
+                          onChange={() => handleDayTimeUpdate(index, "2:30")}
+                          className="form-check-input"
+                        />
+                        2:30
+                      </label>
+                    </div>
+                  </div>
+                }
+              </div>
+            )
+          })}
         </div>
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              value="Yes"
-              checked={canDrive === true}
-              onChange={() => setCanDrive(true)}
-              className="form-check-input"
-            />
-            Yes
-          </label>
+
+        <div className="main-question">
+          <div>
+            <div>
+              Can you drive?
+            </div>
+            <div className="form-check">
+              <label>
+                <input
+                  type="radio"
+                  value="Yes"
+                  checked={canDrive === true}
+                  onChange={() => setCanDrive(true)}
+                  className="form-check-input"
+                />
+                Yes
+              </label>
+            </div>
+
+            <div className="form-check">
+              <label>
+                <input
+                  type="radio"
+                  value="No"
+                  checked={canDrive === false}
+                  onChange={() => setCanDrive(false)}
+                  className="form-check-input"
+                />
+                No
+              </label>
+            </div>
+
+            {canDrive && 
+              <div className="mini-question">
+                <label>
+                How many seats? (including yourself)<br/>
+                  <input
+                    type="text"
+                    value={numSeatsInput}
+                    onChange={(event) => setNumSeatsInput(event.target.value)}
+                    className="inputtext"
+                    placeholder="input seats"
+                  />
+                </label>
+              </div>
+            }
+
+          </div>
         </div>
-
-        <div className="form-check">
-          <label>
-            <input
-              type="radio"
-              value="No"
-              checked={canDrive === false}
-              onChange={() => setCanDrive(false)}
-              className="form-check-input"
-            />
-            No
-          </label>
+        <div className="button-container">
+          <button className="submit">
+            submit
+          </button>
         </div>
-
-        {canDrive && 
-          <>
-            <label>
-              <input
-                type="text"
-                value={numSeatsInput}
-                onChange={(event) => setNumSeatsInput(event.target.value)}
-              />
-              How many seats? (including yourself)
-            </label>
-          </>
-        }
-
       </div>
-
-      <button>
-        Submit
-      </button>
-
-
     </>
   )
 }

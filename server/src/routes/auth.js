4417b6462287190c09ce10aa7e95a4ede12a25b2
Christopher Milan <chris.m.milan@gmail.com>
2022-10-24 14:45:07 -0700

    server: add /logout endpoint

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -79,4 +79,13 @@ router.get(
   })
 );
 
+router.post("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
 export default router;

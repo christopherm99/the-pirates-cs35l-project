92915f9eb5861a708a8e7a9fbe7ea92748381694
Christopher Milan <chris.m.milan@gmail.com>
2022-11-01 16:21:39 -0700

    server: fix signup converter crashing when no drivers are signed up

--- server/src/api/signup.js
+++ server/src/api/signup.js
@@ -19,7 +19,12 @@ router.post("/", requiresAuth, async (req, res) => {
     queries.push(
       db.query(
         "INSERT INTO sign_ups (user_id, timestamp, car_capacity, leave_time) VALUES (?, ?, ?, ?)",
-        [req.user.user_id, dayjs().toDate(), req.body.car_capacity, dayjs(time).toDate()]
+        [
+          req.user.user_id,
+          dayjs().toDate(),
+          req.body.car_capacity,
+          dayjs(time).toDate(),
+        ]
       )
     );
   });
@@ -31,55 +36,63 @@ router.post("/", requiresAuth, async (req, res) => {
         ORDER BY leave_time ASC, timestamp ASC"
     );
     let queries = [];
-    _.forEach(
-      _.groupBy(drivers, dayjs(drivers.leave_time.toDateString()).toDate()),
-      async (day, drivers) => {
-        let [passengers] = await db.query(
-          "SELECT * FROM sign_ups \
+    if (drivers) {
+      _.forEach(
+        _.groupBy(
+          drivers,
+          dayjs(drivers[0].leave_time.toDateString()).toDate()
+        ),
+        async (day, drivers) => {
+          let [passengers] = await db.query(
+            "SELECT * FROM sign_ups \
             WHERE leave_time leave_time >= ? \
             AND leave_time < ? \
             AND car_capacity = 0 \
             ORDER BY leave_time ASC, timestamp ASC",
-          [dayjs(day).startOf("day").toDate(), dayjs(day).endOf("day").toDate()]
-        );
-        drivers.forEach((driver) => {
-          let carMembers = passengers.splice(0, driver.car_capacity);
-          carMembers.push(driver);
-          passengers.splice(0, driver.car_capacity).forEach((passenger) => {
-            queries.push(
-              db.query(
-                "INSERT INTO practices ( \
+            [
+              dayjs(day).startOf("day").toDate(),
+              dayjs(day).endOf("day").toDate(),
+            ]
+          );
+          drivers.forEach((driver) => {
+            let carMembers = passengers.splice(0, driver.car_capacity);
+            carMembers.push(driver);
+            passengers.splice(0, driver.car_capacity).forEach((passenger) => {
+              queries.push(
+                db.query(
+                  "INSERT INTO practices ( \
                   driver_signup_id, \
                   driver_id, \
                   user_signup_id, \
                   user_id, \
                   leave_time \
                 ) VALUES (?, ?, ?, ?, ?)",
-                [
-                  driver.id,
-                  driver.user_id,
-                  passenger.id,
-                  passenger.user_id,
-                  _.maxBy(passengers, "leave_time"),
-                ]
-              )
-            );
+                  [
+                    driver.id,
+                    driver.user_id,
+                    passenger.id,
+                    passenger.user_id,
+                    _.maxBy(passengers, "leave_time"),
+                  ]
+                )
+              );
+            });
           });
-        });
-        passengers.forEach((passenger) => {
-          queries.push(
-            db.query(
-              "INSERT INTO PRACTICES ( \
+          passengers.forEach((passenger) => {
+            queries.push(
+              db.query(
+                "INSERT INTO PRACTICES ( \
                 user_signup_id, \
                 user_id, \
                 leave_time \
               ) VALUES (?, ?, ?, ?, ?)",
-              [passenger.id, passenger.user_id, passenger.leave_time]
-            )
-          );
-        });
-      }
-    );
+                [passenger.id, passenger.user_id, passenger.leave_time]
+              )
+            );
+          });
+        }
+      );
+    }
     Promise.all(queries)
       .then(() => res.status(200).send("OK"))
       .catch((err) => res.status(500).send(err));

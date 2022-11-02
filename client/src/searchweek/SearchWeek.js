48a2b6f17cbe75d781f7a09df70436119af147a6
Michael Bunte <michaelbunte@Michaels-MacBook-Pro.local>
2022-11-01 17:18:19 -0700

    created base searchweek page

---
+++ client/src/searchweek/SearchWeek.js
@@ -0,0 +1,17 @@
+import "./SearchWeek.css";
+import Navbar from "../navbar/Navbar";
+import DayColumn from "../home/DayColumn";
+import React from "react";
+import axios from "axios";
+import moment from "moment";
+
+export default function SearchWeek() {
+    return (
+        <div>
+            <Navbar/>
+            <label for="week">Search Week</label>
+
+            <input type="week" name="week" id="camp-week" min="2000" required></input>
+        </div>
+    )
+}

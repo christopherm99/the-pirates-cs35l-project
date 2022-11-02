897a54983da597af5a038d2b9dd60be86fe06554
Eric Wu <eric.dianhao.wu@gmail.com>
2022-11-01 18:48:11 -0700

    client:fix bug: use proper field of car object
    Apparently we confused leave_time and depart_time

--- client/src/home/CarpoolChip.js
+++ client/src/home/CarpoolChip.js
@@ -1,10 +1,10 @@
 import React from "react"
 
-const CarpoolChip = ({passengers, depart_time, driver}) => {
+const CarpoolChip = ({passengers, leave_time, driver}) => {
   return (
     <div className="bg-purple-300 pt-1 pb-1.5 px-2.5
     my-1 mx-0 rounded-xl">
-      <div className="font-light text-lg">{depart_time}</div>
+      <div className="font-light text-lg">{leave_time}</div>
       <div className="font-light text-sm mx-0 my-1">{driver.name}'s car</div>
       {
         passengers.map(passenger => (

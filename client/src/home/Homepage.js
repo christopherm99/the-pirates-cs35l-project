8d447d09b3c521d104deac00af83f595fef1403b
Sneha Raghuram <sneha@MacBook-Air.local>
2022-11-08 16:17:12 -0800

    added get axios to Search Members

--- client/src/home/Homepage.js
+++ client/src/home/Homepage.js
@@ -45,8 +45,6 @@ export default function Homepage() {
     )
   })
 
-
-  
   return (
     <>
       <Navbar searchWeekButton submitFormButton loginButton/>
--- client/src/search_members/SearchMembers.js
+++ client/src/search_members/SearchMembers.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Search from './Search';
-import initialDetails from './InitialDetails';
+// import initialDetails from './InitialDetails';
+import axios from "axios";
 
 function SearchMembers() {
+  const [userData, setuserData] = React.useState({});
+  
+  axios.get(`http://localhost:8080/api/users/all`).then((response) => {
+    setuserData(response.data)
+  });
+
   return (
+
     <div className="tc bg-green ma0 pa4 min-vh-100">
-      <Search details={initialDetails}/>
+      {/* <Search details={initialDetails}/> */}
+      <Search details={userData}/>
     </div>
   );
 }

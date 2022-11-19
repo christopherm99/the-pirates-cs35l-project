bba62c1352bbb652aca8e5db1ce09220c4350fe2
Eric Wu <eric.dianhao.wu@gmail.com>
2022-11-18 21:57:17 -0800

    client: Allow users to edit and view bios
    Users can now go to the profile page and change their bios. They can also see other
    users' bios from other users' profile pages. There is a 500 character limit for bios (this is the size of the bio in our sql database).

--- client/src/profile/Profile.js
+++ client/src/profile/Profile.js
@@ -4,16 +4,59 @@ import { useParams } from "react-router-dom";
 
 import "./Profile.css";
 
+const MAX_BIO_LENGTH = 500;
+
 export default function Profile() {
   let { id } = useParams();
+  // user is the user whose profile we are viewing
   const [user, setUser] = React.useState({});
+  // currentUser is the user who is logged in right now.
+  const [ currentUser, setCurrentUser ] = React.useState({});
+  const [ currentBio, setCurrentBio ] = React.useState("");
+  const [ isEditing, setIsEditing ] = React.useState(false);
+  const [ errorMessage, setErrorMessage ] = React.useState("");
+
+  const isViewingSelf = JSON.stringify(currentUser) !== "{}" && currentUser.id === user.id;
 
   React.useEffect(() => {
     axios.get(`/api/users/${id}`).then((response) => {
-      console.log(response.data);
       setUser(response.data);
     });
+    axios.get(`/api/users`).then((response) => {
+      // querying without an id field gives the currently logged in user.
+      console.log(response);
+      if (response.status === 200) {
+        setCurrentUser(response.data);
+        if (response.data.bio !== null) {
+          setCurrentBio(response.data.bio);
+        }
+      }
+    });
   }, []);
+
+  const updateBio = () => {
+    if (currentBio.length >= MAX_BIO_LENGTH) {
+      setErrorMessage(`There is a 500 character limit for the bio (your text is ${currentBio.length} chars).`);
+      return;
+    }
+
+    axios.post('/api/users', {
+      user_id: currentUser.id,
+      bio: currentBio,
+    })
+    .then(function () {
+      setIsEditing(false);
+      setUser({
+        ...user,
+        bio: currentBio,
+      });
+      setErrorMessage("");
+    })
+    .catch(function (error) {
+      setErrorMessage(`There was an error saving your bio.`);
+    });
+  }
+
   let items = Object.keys(user)
     .filter((key) => key !== "pfp" && key !== "name" && key !== "id")
     .map((key) => {
@@ -34,8 +77,53 @@ export default function Profile() {
           className="rounded-full h-16"
           src={user.pfp}
           referrerPolicy="no-referrer"
+          alt="user profile"
         />
       </div>
+      {((!isViewingSelf && user.bio) || (isViewingSelf && !isEditing)) && 
+        <div className="flex flex-col bg-white p-10 rounded-xl place-self-center mt-10 hover:drop-shadow-lg whitespace-pre-wrap max-w-md">
+          <div className="font-medium  text-2xl mt-0 mb-2 text-blue-600">
+            {user.name}'s Bio:
+          </div>
+          <div>
+            {user.bio}
+          </div>
+          {isViewingSelf && 
+            <button 
+              className="mt-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 w-fit rounded"
+              onClick={() => setIsEditing(true)}
+            >
+              Edit
+            </button>
+          }
+        </div>
+      }
+      {(isViewingSelf && isEditing) && 
+        <div className="flex flex-col bg-white p-10 rounded-xl place-self-center mt-10 hover:drop-shadow-lg whitespace-pre-wrap max-w-lg">
+          <div className="font-medium  text-2xl mt-0 mb-2 text-blue-600">
+            Edit your Bio:
+          </div>
+          <textarea
+            className="block p-2.5 w-full mb-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value={currentBio}
+            onChange={({ target: { value } }) => setCurrentBio(value)}
+            rows="15"
+            cols="200"
+          >
+          </textarea>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 w-fit rounded"
+            onClick={() => updateBio()}
+          >
+            Save
+          </button>
+          {errorMessage &&
+            <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+              {errorMessage}
+            </div>
+          }
+        </div>
+      }
     </>
   );
 }

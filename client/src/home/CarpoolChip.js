b2e9e583f708830e4de54beef17ea8987f089fa8
Eric Wu <eric.dianhao.wu@gmail.com>
2022-11-27 14:44:12 -0800

    refactor css for daycolumn
    remove duplicate css

--- client/src/home/CarpoolChip.js
+++ client/src/home/CarpoolChip.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 const CarpoolChip = ({ passengers, leave_time, driver }) => {
+  // This component is a single chip which represents a single car leaving
+  // on a particular day.
+  // There may be multiple CarpoolChip components under a single DayColumn.
+
   const date = new Date(leave_time);
 
   const options = { hour: "numeric", minute: "numeric" };
 
   return (
     <div
-      className="bg-purple-300 pt-1 pb-1.5 px-2.5
-    my-1 mx-0 rounded-xl"
+      className="bg-purple-300 pt-1 pb-1.5 px-2.5 my-1 mx-0 rounded-xl"
     >
       <div className="font-light text-lg">
         {date.toLocaleDateString(undefined, options)}
--- client/src/home/DayColumn.js
+++ client/src/home/DayColumn.js
@@ -1,15 +1,24 @@
 import moment from "moment";
 import React from "react";
 import CarpoolChip from "./CarpoolChip";
+import "./DayColumn.css";
 export default function DayColumn({ dateString, carsLeaving }) {
   // dateString should be an ISO datestring such as "2022-10-11"
+  // This component is a column representing a single day's cars.
+  // This is rendered on the home page as well as the search-past-weeks page
+
   let dateMomentObj = moment(dateString);
 
+  // styles are from Homepage.css
   return (
     <div className="day">
-      <div className="day-date">
-        <div className="date-num">{dateMomentObj.format("D")}</div>
-        <div className="week-day">{dateMomentObj.format("dddd")}</div>
+      <div>
+        <div className="date-num">
+          {dateMomentObj.format("D")}
+        </div>
+        <div className="week-day">
+          {dateMomentObj.format("dddd")}
+        </div>
       </div>
       <div className="carpools">
         {carsLeaving.map((car) => (

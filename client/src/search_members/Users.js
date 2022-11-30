344f6ce09564e1dc2290e7ce2d518be94031e934
Michael Bunte <michaelbunte@Michaels-MacBook-Pro-3.local>
2022-11-30 13:36:07 -0800

    searchusers page now centers correctly, also icons have fun spin

--- client/src/search_members/Users.js
+++ client/src/search_members/Users.js
@@ -6,11 +6,13 @@ const Users = ({ user }) => {
   }
 
   return (
-    <article className="box" onClick={() => handleClick(user.id)}>
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
-      <img className="image" src={user.pfp} referrerpolicy="no-referrer"></img>
-    </article>
+    <div className="box-container">
+      <article className="box" onClick={() => handleClick(user.id)}>
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <img className="image" src={user.pfp} referrerpolicy="no-referrer"></img>
+      </article>
+    </div>
   );
 };
 export default Users;

774980d02b7f1a3774fba3b284a2b62a5bab60d1
Michael Bunte <michaelbunte@Michaels-MacBook-Pro-3.local>
2022-11-29 16:13:31 -0800

    spruced up user page

--- client/src/home/CarpoolChip.js
+++ client/src/home/CarpoolChip.js
@@ -1,24 +1,21 @@
 import React from "react";
 
 const CarpoolChip = ({ passengers, leave_time, driver }) => {
-  // This component is a single chip which represents a single car leaving
-  // on a particular day.
-  // There may be multiple CarpoolChip components under a single DayColumn.
-
   const date = new Date(leave_time);
 
   const options = { hour: "numeric", minute: "numeric" };
 
   return (
     <div
-      className="bg-purple-300 pt-1 pb-1.5 px-2.5 my-1 mx-0 rounded-xl"
+      className="bg-purple-300 pt-1 pb-1.5 px-2.5
+    my-1 mx-0 rounded-xl"
     >
       <div className="font-light text-lg">
         {date.toLocaleDateString(undefined, options)}
       </div>
       <div className="font-light text-sm mx-0 my-1">{driver.name}'s car</div>
       {passengers.map((passenger) => (
-        <div className="font-light text-xs mx-0 my-[0.5] leading-3">
+        <div className="font-light text-xs mx-0 my-[0.5] leading-3 py-0.5">
           {passenger.name}
         </div>
       ))}
